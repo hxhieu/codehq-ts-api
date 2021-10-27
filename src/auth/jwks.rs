6a26c1601b0a59033f01caa144bc3d9c44f3de7c
Hugh Hoang <hugh.hoang@gmail.com>
2021-10-27 13:33:01 +1300

    refactor: use Result map_err to reduce the nesting

--- src/auth/jwks.rs
+++ src/auth/jwks.rs
@@ -2,6 +2,7 @@ extern crate alcoholic_jwt;
 
 use alcoholic_jwt::JWKS;
 use dotenv::dotenv;
+use log::info;
 use openidconnect::{
     core::CoreProviderMetadata,
     http::{HeaderMap, Method},
@@ -23,42 +24,36 @@ pub async fn get_jwks() -> Result<JWKS, String> {
 
     // Otherwise fetch the JWKS for the 1st time
 
+    info!("No JWKS in cache, fetch it now");
+
     dotenv().ok(); // Read from .env if there is one
 
-    match IssuerUrl::new(env::var("CODEHQ_TS_API_AUTH_AUTHORITY").unwrap_or("".to_string())) {
-        Ok(authority_url) => {
-            let provider_metadata = CoreProviderMetadata::discover(&authority_url, http_client);
-            match provider_metadata {
-                Ok(metadata) => {
-                    // HACK: Can't the reqwest to work by itself, so
-                    // using openidconnect http_request instead...
-                    let request = HttpRequest {
-                        url: metadata.jwks_uri().url().to_owned(),
-                        method: Method::GET,
-                        headers: HeaderMap::new(),
-                        body: Vec::new(),
-                    };
-                    match http_client(request) {
-                        Ok(res) => {
-                            match serde_json::from_slice::<JWKS>(&res.body) {
-                                Ok(key_store) => {
-                                    // Update the cache
-                                    let mut new_key_store = KEY_STORE.write().unwrap();
-                                    *new_key_store = Some(key_store.clone());
-                                    // Return cache value
-                                    return Ok(key_store);
-                                }
-                                Err(err) => return Err(err.to_string()),
-                            }
-                        }
-                        Err(err) => return Err(err.to_string()),
-                    }
-                }
-                Err(err) => return Err(err.to_string()),
-            }
-        }
-        Err(err) => return Err(err.to_string()),
-    }
+    let authority_url =
+        IssuerUrl::new(env::var("CODEHQ_TS_API_AUTH_AUTHORITY").unwrap_or("".to_string()))
+            .map_err(|err| format!("Invalid authority URL. {}", err))?;
+
+    let metadata = CoreProviderMetadata::discover(&authority_url, http_client)
+        .map_err(|err| format!("Failed to fetch authority metadata. {}", err))?;
+
+    // HACK: Can't the reqwest to work by itself, so
+    // using openidconnect http_request instead...
+    let request = HttpRequest {
+        url: metadata.jwks_uri().url().to_owned(),
+        method: Method::GET,
+        headers: HeaderMap::new(),
+        body: Vec::new(),
+    };
+
+    let response = http_client(request)
+        .map_err(|err| format!("Failed to fetch JWKS from the authority. {}", err))?;
+    let key_store = serde_json::from_slice::<JWKS>(&response.body)
+        .map_err(|err| format!("Failed to deserialise the JWKS. {}", err))?;
+
+    // Update the cache
+    let mut new_key_store = KEY_STORE.write().unwrap();
+    *new_key_store = Some(key_store.clone());
+    // Return cache value
+    return Ok(key_store);
 }
 
 pub async fn validate_token(token: &str, key_store: &JWKS) -> Result<bool, String> {
