eca2bb1479aa63cb34c04f5a18f397f27aa1cfb6
Hugh Hoang <hugh.hoang@codehq.nz>
2021-10-29 11:54:24 +1300

    chore: use all IPs for web server

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> std::io::Result<()> {
                     .service(routes::me::weekly_timesheet::get_date),
             )
     })
-    .bind("127.0.0.1:8080")?
+    .bind("[::]:8080")?
     .run()
     .await
 }
