459e4992db5f594d4037d61654a1feffcd187916
Hugh Hoang <hugh.hoang@codehq.nz>
2021-11-01 17:19:09 +1300

    feat: set response content_type to JSON

--- src/routes/me/weekly_timesheet.rs
+++ src/routes/me/weekly_timesheet.rs
@@ -7,7 +7,9 @@ async fn get_now(req: HttpRequest) -> impl Responder {
     let today = Local::today().format("%d%m%Y").to_string();
     let user = RequestContext::new(&req).user;
     match get_weekly_timesheet(&user, &today) {
-        Ok(result) => HttpResponse::Ok().body(result),
+        Ok(result) => HttpResponse::Ok()
+            .content_type("application/json")
+            .body(result),
         Err(err) => HttpResponse::InternalServerError().body(err),
     }
 }
@@ -16,7 +18,9 @@ async fn get_now(req: HttpRequest) -> impl Responder {
 async fn get_date(web::Path(date): web::Path<String>, req: HttpRequest) -> impl Responder {
     let user = RequestContext::new(&req).user;
     match get_weekly_timesheet(&user, &date) {
-        Ok(result) => HttpResponse::Ok().body(result),
+        Ok(result) => HttpResponse::Ok()
+            .content_type("application/json")
+            .body(result),
         Err(err) => HttpResponse::InternalServerError().body(err),
     }
 }
